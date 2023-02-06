f3e6d3d00c3aaa2e605a5d5b6cbb0cf7163876bc
IvanJevtic9 <ijevtic459@gmail.com>
2023-02-06 12:41:00 +0100

    [Catalog] Added first endpoint

---
+++ src/eShopApp.Catalog/eShopApp.Catalog.App/Extensions/ConfigurationExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace eShopApp.Catalog.App.Extensions
+{
+    public static class ConfigurationExtensions
+    {
+        public static IServiceCollection AddSwagger(this IServiceCollection services)
+        {
+            services.AddEndpointsApiExplorer();
+
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "EShop Catalog API",
+                    Version = "v1",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Ivan Jevtic",
+                        Email = "ijevtic459@gmail.com"
+                    }
+                });
+            });
+
+            return services;
+        }
+    }
+}
--- src/eShopApp.Catalog/eShopApp.Catalog.App/Program.cs
+++ src/eShopApp.Catalog/eShopApp.Catalog.App/Program.cs
@@ -1,12 +1,25 @@
 
+using eShopApp.Catalog.App.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSwagger();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+
+app.UseSwagger();
+app.UseSwaggerUI(options =>
 {
-    app.UseExceptionHandler("/Error");
-}
+    options.SwaggerEndpoint($"/swagger/v1/swagger.json", "EShop Catalog API service");
+});
+
+app.UseHttpsRedirection();
+
+app.MapGet("time", async () =>
+{
+    return Results.Ok(DateTime.UtcNow);
+});
 
 app.Run();
